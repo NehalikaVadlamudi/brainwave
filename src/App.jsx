57634d832f60c503e7fce4d8c5eee169fa7c6ba6
Nehalika Vadlamudi <nehalika100@gmail.com>
2024-11-04 00:36:36 -0500

    added all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./assets/svg/ButtonGradient";
+import Services from "./components/Services";
+import Roadmap from "./components/Roadmap";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -10,6 +17,13 @@ const App = () => {
           Something
         </Button> */}
         <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,7 @@
+import Button from "../components/Button";
+export default {
+  title: "Button",
+  component: Button,
+};
+
+export const Red = () => <Button label="Press me" ></Button>
